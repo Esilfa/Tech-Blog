f741702c2362a0785dfa07dd4da314eb72152571
Estefany Silfa <esilfa3@outlook.com>
2022-01-31 20:58:23 -0500

    update server

--- server.js
+++ server.js
@@ -5,5 +5,21 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
-
+const helpers = require('./utils/helpers');
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Pink Sky',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
