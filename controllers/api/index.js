d1076e4b173d515742703280ddfbce465094c214
Estefany Silfa <esilfa3@outlook.com>
2022-02-01 20:44:26 -0500

    connect controllers

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routs');
+const commentRoutes = require('./comment-routes');
+
+
+
+module.exports=router;
