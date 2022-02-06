61eb30829716a94db72389f6b94a8cfec060f97e
Estefany Silfa <esilfa3@outlook.com>
2022-02-06 18:55:45 -0500

    update config

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+let sequelize;
+
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+}
+else {
     sequelize = new Sequelize(
-        "techBlog_db",
-        process.env.DB_USER,
+        process.env.DB_NAME, process.env.DB_USER,
         null,
-
         {
             host: 'localhost',
             dialect: 'mysql',
