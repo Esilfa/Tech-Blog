dc9346f2896edbd1f78ea7f2cdc35e45e7ec3279
Estefany Silfa <esilfa3@outlook.com>
2022-02-25 12:18:51 -0500

    update connection

--- server.js
+++ server.js
@@ -7,35 +7,37 @@ const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
 
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//       cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+const sess = {
+    secret: 'Pink Sky',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 app.engine("handlebars", hbs.engine);
 
 app.set('view engine', 'handlebars');
 
 //   add app use
-// app.use(session(sess));
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(  '/Public', express.static(path.join(__dirname, "Public") ));
 
-app.use(require("./controllers/index"));
 
-// app.use(routes);
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('server is running'));
